2d388aefae74717d2e4273db80d2edf625e27651
Elen Ardiyanto <79313853+elenardi@users.noreply.github.com>
2022-03-17 10:26:21 +0700

    convertion type data

---
+++ convertion.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai32 = 129
+	var nilai64 int64 = int64(nilai32)
+
+	//missing convertion
+	var nilai8 int8 = int8(nilai32)
+
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+	fmt.Println(nilai8)
+}
