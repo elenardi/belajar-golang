e4b82999c7f7d2cb1f0d35fa41b378c683132f7e
Elen Ardiyanto <79313853+elenardi@users.noreply.github.com>
2022-03-17 09:44:35 +0700

    type data string

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Elen")
+	fmt.Println("Elen Ardi")
+	fmt.Println("Elen Ardiyanto")
+	fmt.Println(len("Elen"))
+	fmt.Println("Elen Ardi"[0])
+	fmt.Println("Elen Ardiyanto"[1])
+}
